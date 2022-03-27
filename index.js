5f3e03aac5d8593f0eae30d94bc26bcd207f3acc
Elizabeth Dowling <dowlinge94@gmail.com>
2022-03-27 16:29:15 -0400

    commit tree 47df34fc818b3aa29e47ffe86852e25be44a4bef

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+// const matchyD = drivers.filter(findMatching)
+// function findMatching(cabbie) {
+//     return cabbie === 'Bobby'
+// }
+
+// function findMatching(drivers) {
+//     function f(name, str) {
+//         return name === str
+//     }
+//     return drivers.filter(f)
+// }
+
+function findMatching(name, string) {
+    if (name === string) {
+    return name}
+}
+
+drivers.filter(findMatching)
